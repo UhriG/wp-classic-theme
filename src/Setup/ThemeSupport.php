361417d5e87d47ad57db08522697f901b4ffaf55
Cristian Uhrig <cristianuhrig@gmail.com>
2024-12-27 21:17:19 -0300

    Rename theme support methods for consistency with wpcs

--- src/Setup/ThemeSupport.php
+++ src/Setup/ThemeSupport.php
@@ -16,13 +16,13 @@ class ThemeSupport {
    * @constructor
   */
   public function __construct() {
-    add_action( 'after_setup_theme', [ $this, 'themeSupport' ] );
+    add_action( 'after_setup_theme', [ $this, 'theme_support' ] );
   }
 
   /** 
    * Add theme support.
   */
-  public function themeSupport() {
+  public function theme_support() {
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
   }
